aff9860b40271a45bdb2bc4451726301eba1d9f6
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-07-20 14:51:25 +0530

    removed the unnecessary comments in code

--- 75mergetwostr.js
+++ 75mergetwostr.js
@@ -1,25 +1,6 @@
 // You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 // Return the merged string.
 
-// var mergeAlternately = (word1, word2) =>{
-//     let finWord = "";
-
-//     for(i=0;i<word1.length;i++){
-//          while(i<word2.length){
-//          finWord += `${word1[i]}${word2[i]}`;
-//          }
-//          if(word1 !== undefined && word2 !== undefined)
-//             finWord += `${word1[i]}`;
-         
-//     }
-//     return (finWord);
-// };
-
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {string}
- */
 var mergeAlternately = (word1, word2) => {
     let finWord = "";
     let i = 0;
